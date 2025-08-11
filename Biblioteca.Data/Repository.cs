dede493dc87400a2007bad8d1b1ac6572e36d571
martin-dimitrov07 <m.dimitrov.3065@vallauri.edu>
2025-08-11 22:13:57 +0200

    Inizio implementazione Visualizzazione libri per admin (non ancora funzionante)

--- Biblioteca.Data/Repository.cs
+++ Biblioteca.Data/Repository.cs
@@ -17,9 +17,15 @@ namespace Biblioteca.Data
             _db = new Database(connStr);
         }
 
-        public List<Utente> GetUtenti(string strWhere, SqlParameter[]? parameters = null)
+        public List<Utente>? GetUtenti(string strWhere, SqlParameter[]? parameters = null)
         {
-            string query = $"SELECT * FROM Utenti WHERE {strWhere}";
+            string query = "";
+
+            if (strWhere == "")
+                query = $"SELECT * FROM Utenti";
+            else
+                query = $"SELECT * FROM Utenti WHERE {strWhere}";
+
             var reader = _db.ExecuteReader(query, parameters);
             var utenti = new List<Utente>();
 
@@ -41,6 +47,134 @@ namespace Biblioteca.Data
             }
             return utenti;
         }
+
+        public List<Libro>? GetLibri(string strWhere, SqlParameter[]? parameters = null)
+        {
+            string query = "";
+
+            if (strWhere == "")
+                query = "SELECT * FROM Libri";
+            else
+                query = $"SELECT * FROM Libri WHERE {strWhere}";
+
+            var reader = _db.ExecuteReader(query, parameters);
+            var libri = new List<Libro>();
+
+            while (reader.Read())
+            {
+                var autore = GetAutori($"IdAutore = {reader.GetInt32(2)}", new SqlParameter[] { new SqlParameter("@IdAutore", reader.GetInt32(2)) }).FirstOrDefault();
+                var nazione = GetNazioni($"IdPaese = {reader.GetInt32(4)}", new SqlParameter[] { new SqlParameter("@IdPaese", reader.GetInt32(4)) }).FirstOrDefault();
+                var lingua = GetLingue($"IdLingua = {reader.GetInt32(5)}", new SqlParameter[] { new SqlParameter("@IdLingua", reader.GetInt32(5)) }).FirstOrDefault();
+
+                libri.Add(new Libro
+                {
+                    IdLibro = reader.GetInt32(0),
+                    Titolo = reader.GetString(1),
+                    IdAutore = reader.GetInt32(2),
+                    Autore = autore,
+                    Anno = reader.GetInt32(3),
+                    IdPaese = reader.GetInt32(4),
+                    Paese = nazione,
+                    IdLingua = reader.GetInt32(5),
+                    Lingua = lingua,
+                    Prezzo = reader.GetDecimal(6),
+                    Pagine = reader.GetInt32(7),
+                });
+            }
+
+            if (libri.Count == 0)
+            {
+                return null;
+            }
+
+            return libri;
+        }
+
+        public List<Autore>? GetAutori(string strWhere, SqlParameter[]? parameters = null)
+        {
+            string query = "";
+
+            if (strWhere == "")
+                query = $"SELECT * FROM Autori";
+            else
+                query = $"SELECT * FROM Autori WHERE {strWhere}";
+
+            var reader = _db.ExecuteReader(query, parameters);
+            var autori = new List<Autore>();
+
+            while (reader.Read())
+            {
+                autori.Add(new Autore
+                {
+                    IdAutore = reader.GetInt32(0),
+                    Nome = reader.GetString(1),
+                    Cognome = reader.GetString(2),
+                });
+            }
+
+            if (autori.Count == 0)
+            {
+                return null;
+            }
+
+            return autori;
+        }
+
+        public List<Nazione>? GetNazioni(string strWhere, SqlParameter[]? parameters = null)
+        {
+            string query = "";
+
+            if (strWhere == "")
+                query = $"SELECT * FROM Nazioni";
+            else
+                query = $"SELECT * FROM Nazioni WHERE {strWhere}";
+
+            var reader = _db.ExecuteReader(query, parameters);
+            var nazioni = new List<Nazione>();
+
+            while (reader.Read())
+            {
+                nazioni.Add(new Nazione
+                {
+                    IdPaese = reader.GetInt32(0),
+                    Nome = reader.GetString(1),
+                });
+            }
+
+            if (nazioni.Count == 0)
+            {
+                return null;
+            }
+
+            return nazioni;
+        }
+
+        public List<Lingua>? GetLingue(string strWhere, SqlParameter[]? parameters = null)
+        {
+            string query = "";
+
+            if (strWhere == "")
+                query = $"SELECT * FROM Linque";
+            else
+                query = $"SELECT * FROM Lingue WHERE {strWhere}";
+
+            var reader = _db.ExecuteReader(query, parameters);
+            var lingue = new List<Lingua>();
+            while (reader.Read())
+            {
+                lingue.Add(new Lingua
+                {
+                    IdLingua = reader.GetInt32(0),
+                    Nome = reader.GetString(1),
+                });
+            }
+            if (lingue.Count == 0)
+            {
+                return null;
+            }
+            return lingue;
+        }
+
 
         // NON FUNZIONA
         //public List<T> GetElements<T>(string nameTable, string strWhere, SqlParameter[]? parameters = null) // T è un tipo generico che deve essere specificato al momento della chiamata del metodo
---
+++ Biblioteca.Web/Controllers/LibroController.cs.cs
@@ -0,0 +1,31 @@
+﻿using Biblioteca.Core.Models;
+using Biblioteca.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Reflection;
+
+namespace Biblioteca.Web.Controllers
+{
+    public class LibroController : Controller
+    {
+        private readonly Repository? _repo;
+
+        public LibroController(IConfiguration configuration)
+        {
+            string connStr = configuration.GetConnectionString("DefaultConnection");
+            _repo = new Repository(connStr);
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Libri";
+
+            List<Libro>? libri = _repo.GetLibri("");
+
+            return View(libri);
+        }
+    }
+}
